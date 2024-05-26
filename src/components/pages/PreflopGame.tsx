12e3f67edd13e1b897119e15475744cb9af413c7
kata-yyy <larton-sub@yahoo.co.jp>
2024-05-27 08:04:22 +0900

    ボタンを押すとランダムなハンドを表示

--- src/components/pages/PreflopGame.tsx
+++ src/components/pages/PreflopGame.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import Box from "@material-ui/core/Box";
 import { makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography';
 import Hand from 'components/preflop_game/Hand';
-import { CardObj } from "interfaces/preflop_game";
+import GameStartButton from 'components/preflop_game/GameStartButton';
+import { Action, CardObj, Question } from "interfaces/preflop_game";
+import { card_num, card_suit, pair_nums, suited_nums, off_suited_nums, suited_suits, off_suited_suits, preflop_hand_table } from 'data/preflop_game';
 
 const useStyles = makeStyles((theme: Theme) => ({
   handArea: {
@@ -16,16 +19,122 @@ const useStyles = makeStyles((theme: Theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  actionButtonArea: {
+    height: '100px',
+    width: '800px',
+    margin: '0 auto',
+  },
 }));
 
 const PreflopGame: React.FC = () => {
   const classes = useStyles();
+  const [isGameMsgDisplay, setIsGameMsgDisplay] = useState<boolean>(true);
+  const [isHandDisplay, setIsHandDisplay] = useState<boolean>(false);
+  const [isStartButtonDisplay, setIsStartButtonDisplay] = useState<boolean>(true);
+  const [isActionButtonDisplay, setIsActionButtonDisplay] = useState<boolean>(false);
+  const [isAnswerDisplay, setIsAnswerDisplay] = useState<boolean>(false);
+  const [question, setQuestion] = useState<Question>({ hand: [], correct_action: null });
+  const [answer, setAnswer] = useState<string>("");
+  
   const testHand: CardObj[] = [{suit: "♥️", num: "A"}, {suit: "♣️", num: "2"}];
+
+  const randomHandGenerate = (): void => {
+    const random_num: number = Math.floor(Math.random() * 13);
+    
+    let nums: string;
+    let suits: string[];
+    let action: Action;
+    
+    if (random_num < 1) {
+      nums = pair_nums[Math.floor(Math.random() * pair_nums.length)];
+      suits = off_suited_suits[Math.floor(Math.random() * off_suited_suits.length)];
+      action = preflop_hand_table[nums]
+    } else if (random_num < 7) {
+      nums = off_suited_nums[Math.floor(Math.random() * off_suited_nums.length)];
+      suits = off_suited_suits[Math.floor(Math.random() * off_suited_suits.length)];
+      action = preflop_hand_table[nums + "o"]
+    } else {
+      nums = suited_nums[Math.floor(Math.random() * suited_nums.length)];
+      suits = suited_suits[Math.floor(Math.random() * suited_suits.length)];
+      action = preflop_hand_table[nums + "s"]
+    }
+    
+    setQuestion({
+      hand: [
+        { num: card_num[nums[0]], suit: card_suit[suits[0]] },
+        { num: card_num[nums[1]], suit: card_suit[suits[1]] }
+      ],
+      correct_action: action
+    });
+  };
+
+  const gameMsgDisplay = (): void => {
+    setIsGameMsgDisplay(true);
+  };
+
+  const gameMsgHide = (): void => {
+    setIsGameMsgDisplay(false);
+  };
+
+  const handDisplay = (): void => {
+    setIsHandDisplay(true);
+  };
+
+  const handHide = (): void => {
+    setIsHandDisplay(false);
+  };
+
+  const startButtonDisplay = (): void => {
+    setIsStartButtonDisplay(true);
+  };
+
+  const startButtonHide = (): void => {
+    setIsStartButtonDisplay(false);
+  };
+
+  const actionButtonDisplay = (): void => {
+    setIsActionButtonDisplay(true);
+  };
+
+  const actionButtonHide = (): void => {
+    setIsActionButtonDisplay(false);
+  };
+
+  const answerDisplay = (ans: string): void => {
+    setAnswer(ans);
+    setIsAnswerDisplay(true);
+  };
+
+  const answerHide = (): void => {
+    setIsAnswerDisplay(false);
+  };
 
   return(
     <>
       <Box className={classes.handArea}>
-        <Hand hand={testHand} />
+        {
+          isGameMsgDisplay &&
+          <>
+            <Typography style={{ fontSize: '2rem' }}>
+              プリフロップハンドをランダムに表示します<br></br>
+              正しいアクションを選択して下さい
+            </Typography>
+        </>
+        }
+        {isHandDisplay && <Hand hand={question.hand} />}
+      </Box>
+      <Box className={classes.actionButtonArea}>
+        {
+          isStartButtonDisplay &&
+          <GameStartButton
+            gameMsgHide={gameMsgHide}
+            handDisplay={handDisplay}
+            randomHandGenerate={randomHandGenerate}
+            startButtonHide={startButtonHide}
+            actionButtonDisplay={actionButtonDisplay}
+            answerHide={answerHide}
+          />
+        }
       </Box>
     </>
   );
--- src/interfaces/preflop_game.ts
+++ src/interfaces/preflop_game.ts
@@ -6,3 +6,14 @@ export interface CardObj {
   suit: string;
   num: string;
 };
+
+// 出題するハンドと正しいアクションの組み合わせ
+export interface Question {
+  hand: CardObj[];
+  correct_action: Action;
+};
+
+// プリフロップハンド表
+export interface HandTable {
+  [key: string]: Action;
+};
