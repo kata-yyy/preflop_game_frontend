4cfe981ab0901f1ca8f31379ceba41fe29a87afa
kata-yyy <larton-sub@yahoo.co.jp>
2024-06-09 23:11:46 +0900

    ハンドレンジ表の雛形を作成

--- src/components/pages/Mypage.tsx
+++ src/components/pages/Mypage.tsx
@@ -1,24 +1,79 @@
-import React, { useContext } from "react";
-
-import { AuthContext } from "App";
+import React, { useState } from 'react';
+import 'stylesheets/Mypage.css';
+import { Action } from 'interfaces/preflop_game';
 
 const Mypage: React.FC = () => {
-  const { isSignedIn, currentUser } = useContext(AuthContext);
+  const rows: string[] = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+  const columns: string[] = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+  // 10x10のマスの状態を管理するための配列
+  const [grid, setGrid] = useState<Action[][]>(
+    Array.from({ length: 13 }, () => Array(13).fill("Call"))
+  );
+
+  // 次の文字を取得する関数
+  const getNextCharacter = (current_action: Action): Action => {
+    const actions: Action[] = ["Call", "Raise", "Fold"];
+    const current_index: number = actions.indexOf(current_action);
+    return actions[(current_index + 1) % actions.length];
+  };
+
+  // マスがクリックされたときのイベントハンドラ
+  const handleCellClick = (rowIndex: number, colIndex: number) => {
+    const newGrid = grid.map((row, rIdx) =>
+      row.map((cell, cIdx) => {
+        if (rIdx === rowIndex && cIdx === colIndex) {
+          return getNextCharacter(cell);
+        }
+        return cell;
+      })
+    );
+    setGrid(newGrid);
+  };
+
+  // セルのクラス名を取得する関数
+  const getCellClassName = (value: Action): string => {
+    switch (value) {
+      case "Call":
+        return "cell-call";
+      case "Raise":
+        return "cell-raise";
+      case "Fold":
+        return "cell-fold";
+      default:
+        return '';
+    }
+  };
 
   return (
-    <>
-      {
-        isSignedIn && currentUser ? (
-          <>
-            <h2>作成中</h2>
-          </>
-        ) : (
-          <>
-            <h2>作成中</h2>
-          </>
-        )
-      }
-    </>
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th></th>
+            {columns.map((col, index) => (
+              <th key={index}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, rowIndex) => (
+            <tr key={row}>
+              <th>{row}</th>
+              {columns.map((col, colIndex) => (
+                <td
+                  key={colIndex}
+                  className={getCellClassName(grid[rowIndex][colIndex])}
+                  onClick={() => handleCellClick(rowIndex, colIndex)}
+                >
+                  {grid[rowIndex][colIndex]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
